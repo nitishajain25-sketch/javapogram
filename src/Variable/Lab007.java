4b5b076e3eb59d8b9a0bd8364b68cd67016436aa
Nitisha <nitishajain25@gmail.com>
2026-03-01 17:24:33 +0530

    created variable program

---
+++ src/Variable/Lab007.java
@@ -0,0 +1,19 @@
+package Variable;
+
+public class Lab007 {
+
+	public static void main(String[] args) {
+		int age = 15;
+		String name = "Student Name";
+		char classname = 'a';
+		Boolean isStudent = true ;
+		
+		System.out.println("Age = " + age);
+		System.out.println("Name = " + name);
+		System.out.println("Classname = "+ classname);
+		System.out.println("Boolean = " + isStudent);
+		
+
+	}
+
+}
---
+++ src/Variable/StrLab006.java
@@ -0,0 +1,11 @@
+package Variable;
+
+public class StrLab006 {
+
+	public static void main(String[] args) {
+		String str = "*****";
+		System.out.println("str");
+
+	}
+
+}
