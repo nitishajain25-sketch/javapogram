98f2316a003817dc61d55de421ffad773c387b4b
Your Name <your@email.com>
2026-04-05 22:37:19 +0530

    function

--- src/function/Assignment_calculator.java
+++ src/function/Assignment_calculator.java
@@ -2,4 +2,8 @@ package function;
 
 public class Assignment_calculator {    
 
+    public static void main(String[] args){
+        int result = Math.min(3, 4);
+        System.out.println(result);
+    }
 }
